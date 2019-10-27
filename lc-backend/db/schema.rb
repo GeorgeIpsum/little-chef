a0cc347b7b51af22babe7d91772abc9fe0b7d9d8
Ibrahim Saberi <georgeipsum@gmail.com>
2019-10-27 18:02:27 -0400

    user auth (jwt)

--- lc-backend/db/schema.rb
+++ lc-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_185325) do
+ActiveRecord::Schema.define(version: 2019_10_27_211732) do
 
   create_table "instructions", force: :cascade do |t|
     t.string "text"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_09_29_185325) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.string "created_by"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
